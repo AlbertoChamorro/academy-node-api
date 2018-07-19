3746f69a0d60d84610834cde719210eef7b32dee
Alberto Chamorro <albertchc04@gmail.com>
2018-07-19 15:38:02 -0600

    testing with async await and promise mock

--- test/utils-test.js
+++ test/utils-test.js
@@ -17,3 +17,19 @@ test('it should support async/await', async t => {
 
   t.is(secret, expectedValue)
 })
+
+test('it should support async/await', async t => {
+  let expectedValue = 2
+  let secret = await asyncFunctionToTest(expectedValue)
+
+  t.is(secret, expectedValue)
+})
+
+let asyncFunctionToTest = value => {
+  let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  return new Promise((resolve, reject) => {
+    let findValue = array.filter(item => item === value)[0]
+    return resolve(findValue)
+  })
+}
