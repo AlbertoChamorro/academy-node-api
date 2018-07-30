ced5415721c5eaf82dd53a2a1e204d12fbe4b11c
Alberto Chamorro <albertchc04@gmail.com>
2018-07-29 18:09:21 -0600

    config ava and http-hash for testing services

--- api/test/pictures-test.js
+++ api/test/pictures-test.js
@@ -2,24 +2,21 @@
 
 import uuid from 'uuid-base62'
 import test from 'ava'
-import micro, { send } from 'micro'
+import micro from 'micro'
 import listen from 'test-listen'
 import request from 'request-promise'
+import Pictures from '../pictures'
 
-test('GET /:id', async t => {
+test('GET /pictures/:id', async t => {
   let id = uuid.v4()
-
-  let server = micro(async (req, res) => {
-    send(res, 200, { id })
-  })
+  let server = micro(Pictures)
 
   let url = await listen(server)
-  console.log(`URL *** ${url}`)
-  let body = await request({ uri: url, json: true })
-  console.log(`BODY *** ${body}`)
+  console.log(`URL SEND *** ${url}/pictures/${id}`)
+  let body = await request({ uri: `${url}/pictures/${id}`, json: true })
 
   t.deepEqual(body, { id })
 })
 
-test.todo('POST /')
-test.todo('POST /:id/like')
+test.todo('POST /pictures')
+test.todo('POST /pictures/:id/like')
